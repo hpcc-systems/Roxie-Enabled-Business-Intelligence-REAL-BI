0360ef39dee88b88a1318d2e76075d494bcbd2d9
Chris Human <chrisehuman@gmail.com>
2020-02-10 12:25:42 -0500

    Add logic to identify array and object field types

--- server/src/utils/misc.js
+++ server/src/utils/misc.js
@@ -9,6 +9,10 @@ const getFieldType = value => {
       ? 'number'
       : value == true || value == false
       ? 'boolean'
+      : Array.isArray(value) === true
+      ? 'array'
+      : typeof value === 'object'
+      ? 'object'
       : value.trim().toLowerCase();
 
   return type;
