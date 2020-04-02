d2be87167149e94e0e2c3915944b26ecb0864dc6
Chris Human <chrisehuman@gmail.com>
2020-04-02 14:03:56 -0400

    Add additional values to constants file and update references

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,9 @@
 const initUserObj = { id: null, directory: [], lastDashboard: null };
 
-export { initUserObj };
+// Color array for chart elements
+const chartColors = ['#FF3333', '#30E324', '#FA7411', '#2F56F3', '#F770E7', '#F1D83B', '#BC44F3'];
+
+// Convert PI to radians for pie chart
+const radian = Math.PI / 180;
+
+export { chartColors, initUserObj, radian };
