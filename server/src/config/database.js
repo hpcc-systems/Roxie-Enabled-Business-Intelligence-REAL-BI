dd8f32f05587bb147416ec14f295089cd3c7b47b
Chris Human <chrisehuman@gmail.com>
2020-04-17 10:38:53 -0400

    Fix package-locks and update .env fields for database

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -3,13 +3,13 @@ const path = require('path');
 // Load .env file
 require('dotenv').config({ path: path.join(process.cwd(), '..', '.env') });
 
-const { DB_NAME, DB_PORT, DIALECT, HOST, PASS, USER } = process.env;
+const { DB_DIALECT, DB_HOST, DB_NAME, DB_PASS, DB_USER, DB_PORT } = process.env;
 
 module.exports = {
-  username: USER,
-  password: PASS,
+  username: DB_USER,
+  password: DB_PASS,
   database: DB_NAME,
-  host: HOST,
+  host: DB_HOST,
   port: DB_PORT,
-  dialect: DIALECT,
+  dialect: DB_DIALECT,
 };
