9ca8c7fe790ab0208fb67b87dcf352fe647a64ae
Lohith JR <51131538+Rajesh-Lohith@users.noreply.github.com>
2020-07-13 14:48:27 -0400

    x/y axis sort

--- client/src/components/Chart/Bar/Bar.jsx
+++ client/src/components/Chart/Bar/Bar.jsx
@@ -3,11 +3,12 @@ import ReactG2Plot from 'react-g2plot';
 import { Bar } from '@antv/g2plot';
 
 // Utils
-import { checkForNumber, thousandsSeparator } from '../../../utils/misc';
+import { checkForNumber, thousandsSeparator, sortArr } from '../../../utils/misc';
 
 const BarChart = ({ data, options }) => {
   const { groupBy, xAxis, yAxis, xAxis_Label, yAxis_Label } = options;
 
+  const sortOrder = 'asc';
   const customXLabel = typeof xAxis_Label !== 'undefined' ? xAxis_Label : xAxis;
   const customYLabel = typeof yAxis_Label !== 'undefined' ? yAxis_Label : yAxis;
 
@@ -17,6 +18,9 @@ const BarChart = ({ data, options }) => {
     [xAxis]: checkForNumber(row[xAxis]),
     [yAxis]: checkForNumber(row[yAxis]),
   }));
+
+  // Sort data in ascending order
+  data = sortArr(data, xAxis, sortOrder);
 
   const config = {
     data,
