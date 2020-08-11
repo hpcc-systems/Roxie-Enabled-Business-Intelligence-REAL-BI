dec5d63c35a95a9764d6fedebe40d4908afac20a
Chris Human <chrisehuman@gmail.com>
2020-08-11 10:44:01 -0400

    Add error message to login screen

--- server/src/utils/errHandler.js
+++ server/src/utils/errHandler.js
@@ -1,15 +1,21 @@
 const logger = require('../config/logger');
 
 const errHandler = err => {
-  const { response: { status = 500 } = { status: 500 }, message = 'Internal Error' } = err;
-  let errMsg = message;
+  const { response = {} } = err;
+  const { data, message, status = 500, statusText } = response;
 
+  // Get error message
+  let errMsg = data || message || statusText || 'Internal Error';
+
+  // Update error message if status is a 401
   if (status === 401) {
     errMsg = 'Incorrect Credentials Provided';
   }
 
+  // Log error
   logger.error(errMsg);
 
+  // Return error object
   return { errMsg, status };
 };
 
