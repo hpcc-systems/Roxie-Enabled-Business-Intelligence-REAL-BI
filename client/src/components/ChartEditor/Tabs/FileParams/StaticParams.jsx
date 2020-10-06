b52eee9f883146abaf0efb7197967569124d6f74
Chris Human <chrisehuman@gmail.com>
2020-10-06 13:27:09 -0400

    Rename fields and add tooltips for better UX

--- client/src/components/ChartEditor/Tabs/FileParams/StaticParams.jsx
+++ client/src/components/ChartEditor/Tabs/FileParams/StaticParams.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, TextField } from '@material-ui/core';
+import { Grid, TextField, Tooltip } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   formControl: { marginTop: theme.spacing(1) },
@@ -11,24 +11,38 @@ const StaticFileParams = ({ localState, setParamObj }) => {
   const { formControl } = useStyles();
 
   // Show only certain params
-  const partialParamsArr = params.filter(({ name }) => name === 'Start' || name === 'Count');
+  const startParam = params.filter(({ name }) => name === 'Start');
+  const countParam = params.filter(({ name }) => name === 'Count');
+  const startTooltip = 'The number of the row you wish to begin your dataset. Defaults to the first row (1).';
+  const countTooltip =
+    'The number of rows you wish to return from your dataset. Defaults to complete dataset for queries and 5,000 rows for files and ecl scripts.';
 
   return (
     <Fragment>
-      {partialParamsArr.map(({ name, type, value }, index) => {
-        return (
-          <Grid key={index} item xs={6}>
-            <TextField
-              label={`${name}: ${type}`}
-              value={value || ''}
-              onChange={event => setParamObj(event, name)}
-              autoComplete='off'
-              className={formControl}
-              fullWidth
-            />
-          </Grid>
-        );
-      })}
+      <Grid item xs={6}>
+        <Tooltip title={startTooltip} placement='right'>
+          <TextField
+            label='Starting Row'
+            value={startParam.value || ''}
+            onChange={event => setParamObj(event, 'Start')}
+            autoComplete='off'
+            className={formControl}
+            fullWidth
+          />
+        </Tooltip>
+      </Grid>
+      <Grid item xs={6}>
+        <Tooltip title={countTooltip} placement='right'>
+          <TextField
+            label='Number of Rows'
+            value={countParam.value || ''}
+            onChange={event => setParamObj(event, 'Count')}
+            autoComplete='off'
+            className={formControl}
+            fullWidth
+          />
+        </Tooltip>
+      </Grid>
     </Fragment>
   );
 };
