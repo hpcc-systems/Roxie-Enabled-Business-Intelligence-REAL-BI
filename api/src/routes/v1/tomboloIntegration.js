abc657f4749fec982620e925a8fc3b00e3598873
Kostiantyn Agapov <konstagapov123@gmail.com>
2021-07-08 17:03:21 -0700

    added creating and finding dashboard, dashboards appears in drawers, ready to add table

--- api/src/routes/v1/tomboloIntegration.js
+++ api/src/routes/v1/tomboloIntegration.js
@@ -1,6 +1,17 @@
 const { getClusterByHost, createCluster } = require('../../utils/cluster');
-const { getDashboardByWokspaceAndCluster, createDashboard } = require('../../utils/dashboard');
+const {
+  getDashboardByWokspaceAndCluster,
+  createDashboard,
+  getDashboardByID,
+} = require('../../utils/dashboard');
+const { createDashboardPermission } = require('../../utils/dashboardPermission');
 const { getWorkspacesByUserID, getWorkspaceByID, createWorkspace } = require('../../utils/workspace');
+const {
+  getWorkspaceDirectory,
+  updateWorkspaceDirectory,
+  createWorkspaceDirectory,
+} = require('../../utils/workspaceDirectory');
+const { createWorkspacePermission } = require('../../utils/workspacePermission');
 
 const router = require('express').Router();
 
@@ -9,10 +20,10 @@ router.post('/', async (req, res, next) => {
   req.body = { cluster: { host: 'http://10.173.147.1' } };
 
   try {
-    //1. find or Create TOMBOLO WORKSPACE
     const workspace = await findOrCreateWorkspace(req.user.id);
     const cluster = await findOrCreateCluser(req.body.cluster);
     const dashboard = await findOrCreateDashboard(workspace.id, cluster.id, req.user.id, 'Tombolo');
+    await updateOrCreateWorkspaceDirectory(dashboard, workspace.id, req.user.id); // this one is for directories to appear in drawer
     return res.status(200).send(dashboard);
   } catch (error) {
     return next(error);
@@ -24,12 +35,14 @@ module.exports = router;
 const findOrCreateWorkspace = async userID => {
   const allWorkspacesInfo = await getWorkspacesByUserID(userID);
   if (allWorkspacesInfo) {
-    const searchedWorkspaceInfo = allWorkspacesInfo.find(el => el.name === 'Tombolo');
+    const searchedWorkspaceInfo = allWorkspacesInfo.find(el => el.name === 'Tombolo'); // MAKE SURE UR WORKSPACE NAMED SAME
     if (searchedWorkspaceInfo) {
       return await getWorkspaceByID(searchedWorkspaceInfo.id, userID);
     }
   } else {
-    return await createWorkspace('Tombolo', userID);
+    const workspace = await createWorkspace('Tombolo', userID);
+    await createWorkspacePermission(workspace.id, userID, 'Owner');
+    return workspace;
   }
 };
 
@@ -39,8 +52,31 @@ const findOrCreateCluser = async cluster => {
   return await createCluster(cluster);
 };
 
-const findOrCreateDashboard = async (worspaceID, clusterID, userID, name) => {
-  const dashboard = await getDashboardByWokspaceAndCluster(worspaceID, clusterID, userID, name);
+const findOrCreateDashboard = async (workspaceID, clusterID, userID, name) => {
+  const dashboard = await getDashboardByWokspaceAndCluster(workspaceID, clusterID, userID, name);
   if (dashboard) return dashboard;
-  return await createDashboard({ name, clusterID }, worspaceID);
+  const newDashboardID = await createDashboard({ name, clusterID }, workspaceID);
+  await createDashboardPermission(newDashboardID, userID, 'Owner');
+  return await getDashboardByID(newDashboardID, userID);
+};
+
+const updateOrCreateWorkspaceDirectory = async (dashboard, workspaceID, userID) => {
+  const workspaceDirectoryObj = {
+    id: dashboard.id,
+    name: dashboard.name,
+    favorite: false,
+    shared: false,
+  };
+
+  const workspaceDirectoriesArray = await getWorkspaceDirectory(workspaceID, userID);
+  if (workspaceDirectoriesArray?.directory) {
+    const { directory } = workspaceDirectoriesArray;
+    const duplicate = directory.find(el => el.name === workspaceDirectoryObj.name);
+    if (duplicate) return;
+    const newDirectory = [...directory, workspaceDirectoryObj];
+    await updateWorkspaceDirectory(newDirectory, workspaceID, userID);
+  } else {
+    const directory = [workspaceDirectoryObj];
+    await createWorkspaceDirectory(directory, workspaceID, userID);
+  }
 };
