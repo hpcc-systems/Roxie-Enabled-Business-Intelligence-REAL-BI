928a86753b272517df3ae32a145347246eb55999
Kostiantyn Agapov <konstagapov123@gmail.com>
2021-07-20 16:33:01 -0700

    fixing autocomplete not refetching, debouncing fetch func, add better contast color function

--- client/src/utils/misc.js
+++ client/src/utils/misc.js
@@ -130,29 +130,22 @@ export const getDateParts = date => {
   return { month, day, year };
 };
 
-// https://convertingcolors.com/blog/article/convert_hex_to_rgb_with_javascript.html
-const hexToRgb = hex => {
-  if (hex.length !== 6 && hex.length !== 7) {
-    return [255, 255, 255];
+// https://stackoverflow.com/questions/35969656/how-can-i-generate-the-opposite-color-according-to-current-color
+export function getConstrastTextColor(hex) {
+  if (hex.indexOf('#') === 0) {
+    hex = hex.slice(1);
   }
+  // convert 3-digit hex to 6-digits.
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  if (hex.length !== 6) {
+    return console.log('Invalid HEX color');
+  }
+  const r = parseInt(hex.slice(0, 2), 16),
+    g = parseInt(hex.slice(2, 4), 16),
+    b = parseInt(hex.slice(4, 6), 16);
 
-  const hexVal = hex[0] === '#' ? hex.slice(1, hex.length) : hex;
-  const rgbHex = hexVal.match(/.{1,2}/g);
-  const rgbArr = [parseInt(rgbHex[0], 16), parseInt(rgbHex[1], 16), parseInt(rgbHex[2], 16)];
-
-  return rgbArr;
-};
-
-// https://websolutionstuff.com/post/change-text-color-based-on-background-color-using-javascript
-export const getConstrastTextColor = hex => {
-  const backgroundColorRgb = hexToRgb(hex);
-
-  const color = Math.round(
-    (parseInt(backgroundColorRgb[0]) * 299 +
-      parseInt(backgroundColorRgb[1]) * 587 +
-      parseInt(backgroundColorRgb[2]) * 114) /
-      1000,
-  );
-
-  return color > 125 ? 'black' : 'white';
-};
+  // http://stackoverflow.com/a/3943023/112731
+  return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? '#000000' : '#FFFFFF';
+}
