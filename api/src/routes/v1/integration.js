1a6898b120a87ae1e8ca08c04e67f2ab226cba61
Kostiantyn Agapov <konstagapov123@gmail.com>
2021-08-25 10:59:22 -0700

    not passing file from tombolo

--- api/src/routes/v1/integration.js
+++ api/src/routes/v1/integration.js
@@ -12,6 +12,7 @@ const router = require('express').Router();
 
 router.post('/', async (req, res, next) => {
   const userRole = req.body.editingAllowed ? 'Owner' : 'Read-Only';
+  const file = req.body?.filename ? req.body.filename.trim() : null;
   try {
     const user = await getUserByEmail(req.body.user.email.trim());
     const workspace = await findOrCreatePublicWorkspace(user.id, req.body.workspaceName.trim(), userRole);
@@ -21,14 +22,14 @@ router.post('/', async (req, res, next) => {
       cluster.id,
       user.id,
       req.body.dashboardName.trim(),
-      req.body.filename.trim(),
+      file,
       userRole,
     );
     await addDashboardAsOpenDashboard(dashboard.id, workspace.id, user.id);
     await updateOrCreateWorkspaceDirectory(dashboard, workspace.id); // this one is for directories to appear in drawer
 
     const url = SHARE_URL || 'http://localhost:3000';
-    const link = `${url}/workspace/${workspace.id}/${dashboard.id}/${req.body.filename.trim()}/`;
+    const link = `${url}/workspace/${workspace.id}/${dashboard.id}/${file || ''}`;
     return res.status(200).send({ workspaceUrl: link });
   } catch (error) {
     return next(error);
