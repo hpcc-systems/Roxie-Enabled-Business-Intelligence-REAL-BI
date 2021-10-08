529b381f1a54ae374e07ef83d28293c241edb891
Kostiantyn Agapov <konstagapov123@gmail.com>
2021-10-08 12:05:33 -0700

    revert email notification changes, fixed bug with shared workpsace_directory

--- api/src/routes/v1/share.js
+++ api/src/routes/v1/share.js
@@ -10,7 +10,7 @@ router.post('/', [validateWorkspaceShare(), validate], async (req, res, next) =>
     user: { id: userID },
   } = req;
 
-  const { TRANSPORT_HOST, TRANSPORT_PORT, SHARE_URL } = process.env;
+  const { TRANSPORT_HOST, TRANSPORT_PORT } = process.env;
 
   try {
     const { permission = 'Read-Only' } = await getWorkspaceByID(workspaceID, userID);
@@ -39,11 +39,8 @@ router.post('/', [validateWorkspaceShare(), validate], async (req, res, next) =>
 
       await sendShareWorkspaceEmail(shareID, workspaceID, email, newUser);
     }
-    // TODO: temp setting
-    const url = `${SHARE_URL}/workspace/${workspaceID}`;
-    res.send(url);
-    // TODO:uncomment later
-    // res.status(200).end();
+
+    res.status(200).end();
   } catch (error) {
     return next(error);
   }
