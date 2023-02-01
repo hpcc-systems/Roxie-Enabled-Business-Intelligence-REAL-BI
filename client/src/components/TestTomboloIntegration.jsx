7477c460b61dbff0349141c6c236eecc74a1c4e3
yadhap Dahal <yadhap.dahal@lexisnexisrisk.com>
2023-02-01 13:29:47 -0500

    removed unused code- hpcc url

--- client/src/components/TestTomboloIntegration.jsx
+++ client/src/components/TestTomboloIntegration.jsx
@@ -12,42 +12,10 @@ function TestTomboloIntegration() {
 
       {
         user: { email: 'admin@lexisnexisrisk.com' },
-        // user: { email: 'Kostiantyn.Agapov@lexisnexisrisk.com' }, // temp hack to avoid passport middleware, this key is added by passport
         editingAllowed: true,
-        // cluster: {
-        //   name: 'http://10.173.147.1',
-        //   host: 'http://10.173.147.1',
-        //   infoPort: '8010',
-        //   dataPort: '8002',
-        // },
-        // filename: 'temp::hfs::taxidata_small',
         workspaceName: 'Tombolo', // Arbitrary name
         dashboardName: 'Workunits123', // this dashboard will show only files form this cluster. if you have files from other cluster, create new dash like "Tombolo ([cluster Name])"
       },
-      // {
-      //   user: { email: 'admin@lexisnexisrisk.com' },
-      //   cluster: {
-      //     name: 'http://10.173.147.1',
-      //     host: 'http://10.173.147.1',
-      //     infoPort: '8010',
-      //     dataPort: '8002',
-      //   },
-      //   filename: 'asdf::aaa::fake_people.csv_thor',
-      //   workspaceName: 'Tombolo',
-      //   dashboardName: 'This Dash is NEW!', //title
-      // },
-      // {
-      //   user: { email: 'Kostiantyn.Agapov@lexisnexisrisk.com' }, // temp hack to avoid passport middleware, this key is added by passport
-      //   cluster: {
-      //     name: 'http://10.173.147.1',
-      //     host: 'http://alpha_qc_thor_esp.risk.regn.net',
-      //     infoPort: '8010',
-      //     dataPort: '8002',
-      //   },
-      //   filename: 'base::personcontext::exemption::20210701031504prod::productid',
-      //   workspaceName: 'Tombolo', // Arbitrary name
-      //   dashboardName: 'Tombolo (alpha_qc)', // maybe need to add cluster name like Tombolo (new_cluster)
-      // },
     ); // PROVIDE BODY FOR TESTING
     console.log('result :>> ', result.data);
     setRespond(result.data);
